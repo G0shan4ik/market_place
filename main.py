1d42622d8b411aa7f6929dce9a512cd26ce45c27
Egor <egorkononec64@gmail.com>
2025-01-30 22:40:19 +0300

    [-] -

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import asyncio
 from market.database.sql.core import init
+from subprocess import run
+from market.api.core import app
 
 
-def start_test():
-    asyncio.run(init())
+# def start_test():
+#     asyncio.run(init())
+
+
+def start_dev():
+    run(["uvicorn", "main:app", "--reload", "--host=127.0.0.1", "--port=8000", "--reload"])
+
+
+if __name__ == '__main__':
+    init()
--- market/database/sql/core.py
+++ market/database/sql/core.py
@@ -1,11 +1,24 @@
-from .config import settings
 from sqlalchemy.orm import DeclarativeBase, declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from loguru import logger
 
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+USER = getenv("POSTGRES_USER") or None
+PASSWORD = getenv("POSTGRES_PASSWORD") or None
+HOST = getenv("POSTGRES_HOST")
+PORT = getenv("POSTGRES_PORT")
+DB = getenv("POSTGRES_DB")
+
+uri = f"{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+print(uri)
 
 engine = create_async_engine(
-    url=settings.db_url_asyncpg,
+    url=f"postgresql+psycopg_async://{uri}",
     echo=True
 )
 
