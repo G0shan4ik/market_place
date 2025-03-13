7438f56aeb1f71596932e109126c4461cd33e066
G0shan4ik <egorkononec64@gmail.com>
2025-03-13 22:01:12 +0300

    [-] -

--- market/api/routes/users.py
+++ market/api/routes/users.py
@@ -52,7 +52,10 @@ async def update_user_data(
     user: UserRequestUpdate,
     user_db: Annotated[UserService, Depends(sql_helper_factory(UserService))]
 ):
-    await user_db.update_user(user_id, **user.model_dump(exclude_unset=True))
+    status: bool = await user_db.update_user(user_id, **user.model_dump(exclude_unset=True))
+    return {
+        'status': status
+    }
 
 
 @user_router.post(
