07a7d38c2cd6c1b7987d3ccdb6f0309d2822745c
G0shan4ik <egorkononec64@gmail.com>
2025-03-14 18:56:29 +0300

    [-] -

--- market/api/routers/__init__.py
+++ market/api/routers/__init__.py
@@ -3,3 +3,4 @@ from .products import product_router
 from .reviews import review_router
 from .payments import payment_router
 from .orders import order_router
+from .categories import category_router
